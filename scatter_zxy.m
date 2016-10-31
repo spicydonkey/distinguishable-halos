3a05068cc8870faf8d196023d55c006df47d9b6c
spicydonkey <dk.shin1992@gmail.com>
2016-11-01 00:34:49 +1100

    add user select color feature

--- scatter_zxy.m
+++ scatter_zxy.m
@@ -1,9 +1,13 @@
 % SCATTER PLOT ZXY ARRAY
 % DKS 31/10/2016
 
-function scatter_zxy(FIG,ZXY_ARRAY)
+function scatter_zxy(FIG,ZXY_ARRAY,COLOR)
+if ~exist('COLOR','var')
+    COLOR='k';  % default COLOR is black
+end
+
 figure(FIG); hold on;
-scatter3(ZXY_ARRAY(:,2),ZXY_ARRAY(:,3),ZXY_ARRAY(:,1),1,'k.');
+scatter3(ZXY_ARRAY(:,2),ZXY_ARRAY(:,3),ZXY_ARRAY(:,1),1,[COLOR,'.']);
 axis vis3d; axis equal;
 xlabel('X'); ylabel('Y'); zlabel('Z');
 end
