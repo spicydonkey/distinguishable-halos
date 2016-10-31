395142ee526dda9814d10455c32634459a5e7dfb
David <dk.shin1992@gmail.com>
2016-10-31 17:55:24 +1100

    do error checks

--- txy_importer.m
+++ txy_importer.m
@@ -1,9 +1,36 @@
-function  data=txy_importer(filepath,filenum_str)
+function  data=txy_importer(filepath,filenum)
 %this function imports txy files faster by using strongly a matched import
 %single precision 3 digits
 %simply pass the filepath and file number (as string)
+% 31/10/16 DKS - filenum will accept integer and int string - error raised
+%   for null output and non-int/str filenum
 
-fileID = fopen([filepath,'_txy_forc',filenum_str,'.txt'],'r');
+%% ERROR CHECK
+% Check filenum variable type
+if isa(filenum,'char')
+    if isempty(str2double(filenum))
+        data=[];
+        warning(['filenum: ',filenum,' is not an integer string - returning null data']);
+        return
+    else
+        filenum=str2double(filenum);
+    end
+elseif ~isa(filenum,'double')
+    % error on data type
+    data=[];
+    warning(['filenum is not an int or int str - returning null data']);
+    return
+end
+
+% Check filenum is an integer
+if ~(filenum==floor(filenum))
+    data=[];
+    warning(['filenum: ',num2str(filenum),' is not an integer - returning null data']);
+    return
+end
+
+%% LOAD DATA
+fileID = fopen([filepath,'_txy_forc',num2str(filenum),'.txt'],'r');
 data = textscan(fileID,'%f32%f32%f32',...%f32
     'Delimiter', ',', ...
     'MultipleDelimsAsOne',0,...
@@ -13,4 +40,5 @@ data = textscan(fileID,'%f32%f32%f32',...%f32
     'EndOfLine','\n');
 fclose(fileID);
 data=data{1};
+
 end
