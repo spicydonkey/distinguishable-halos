d92e5527880ead361a096bb9f49e5e2e9e4b2c5e
David <dk.shin1992@gmail.com>
2016-11-01 17:22:50 +1100

    create a function to do zxy translation

---
+++ zxy_translate.m
@@ -0,0 +1,15 @@
+function ZXY_OUT = zxy_translate(ZXY_IN,DISP)
+% Translates uniformly all ZXY-count of each shot by the correponding displacement vector
+% DKS 1/11/16
+%
+% I/O
+% ZXY_IN: should be a Nx1 cell of ZXY-array
+% DISP: Nx1 cell of a single ZXY displacement vector
+
+nShot=size(ZXY_IN,1);
+
+ZXY_OUT=cell(nShot,1);
+for i=1:nShot
+    nCount=size(ZXY_IN{i},1);
+    ZXY_OUT{i}=ZXY_IN{i}-repmat(DISP{i},[nCount,1]);
+end
