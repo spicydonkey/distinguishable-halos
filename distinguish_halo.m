887d779a1848184d2ea8e3b15c1a913604d0e4dc
David <dk.shin1992@gmail.com>
2016-11-01 13:26:45 +1100

    add halo capturing feature

--- distinguish_halo.m
+++ distinguish_halo.m
@@ -1,4 +1,4 @@
-function [HALO,BEC] = distinguish_halo(CONFIGS,VERBOSE)
+function [HALO,BEC,CULLED] = distinguish_halo(CONFIGS,VERBOSE)
 % Processes the raw TXY data to identify distinguishable halos and their
 % properties
 %
@@ -24,13 +24,18 @@ end
 
 % Parse input
 f_path = CONFIGS.files.path;
-f_id = CONFIGS.files.idok;     % only analyse ok files
+f_id = CONFIGS.files.idok;      % only analyse ok files
+
+R_tail=CONFIGS.bec.rtail;       % estimated ratio of tail to BEC
+R_halo=CONFIGS.halo.dR;         % fractional error around estimtaed halo rad for cropping (TODO - sensitivity?)
 
 % Initialise variables
 HALO.zxy=cell(length(f_id),2);  % halos
 HALO.cent=cell(length(f_id),2); % centre of halos
 BEC.zxy=cell(length(f_id),2);   % BECs
 BEC.cent=cell(length(f_id),2);  % centre of BECs
+CULLED.tail.zxy=cell(length(f_id),2);   % BEC tails
+CULLED.fuzz.zxy=cell(length(f_id),1);   % halo fuzz + background counts - 1D collated since no source to distinguish
 
 %% halo processing
 for i=1:length(f_id)
@@ -53,7 +58,10 @@ for i=1:length(f_id)
     end
     
     %% Locate condensates
+    % Locate condensate by cropping a ball around estimated centres and
+    %   radius and AVERAGE count positions
     zxy_bec=cell(2,1);  % BEC counts
+    
     for i_cond=1:2
         zxy_temp=zxy_shot-repmat(CONFIGS.bec.pos{i_cond},[length(zxy_shot),1]); % relocate centre to approx BEC position
         zxy_temp=sum(zxy_temp.^2,2);            % evaluate radial distances
@@ -64,22 +72,41 @@ for i=1:length(f_id)
     end
     
     %% Clean around condensates
+    % There is still spherical tail around the condensate capturing sphere which is
+    %   much stronger than scattered counts
+    % Must be done after locating condensates since the clean-up windows
+    %   for two condensates will significantly overlap
+    zxy_tail=cell(2,1);
+    
     for i_cond=1:2
-        % TODO - remove counts near condensates: ~1.3*rad_window
+        zxy_temp=zxy_shot-repmat(CONFIGS.bec.pos{i_cond},[length(zxy_shot),1]); % relocate centre to approx BEC position
+        zxy_temp=sum(zxy_temp.^2,2);            % evaluate radial distances
+        ind_tail=zxy_temp<((R_tail{i_cond}*CONFIGS.bec.Rmax{i_cond})^2);  % tail counts index
+        zxy_tail{i_cond}=zxy_shot(ind_tail,:);  % counts in BEC tail
+        zxy_shot=zxy_shot(~ind_tail,:);         % pop tail out
     end
     
     %% Capture halos
-    % assuming BEC centre lies on halo's Z-extremity and estimating halo radii
+    % assuming BEC centre lies on halo's Z-extremity and estimating halo
+    % radii, apply radial crop
+    zxy_halo=cell(2,1); % temp for halo counts
+    
     for i_halo=1:2
-        % add/subtract estimated halo radius in Z
+        % add/subtract estimated halo radius in Z: HALO 1 should be the
+        %   non-magnetic Raman outcoupled atoms - Halo must sit "ABOVE" the
+        %   BEC
         HALO.cent{i,i_halo}=BEC.cent{i,i_halo}+((-1)^(i_halo+1))*[1,0,0]*CONFIGS.halo.R{i_halo};
         zxy_temp=zxy_shot-repmat(HALO.cent{i,i_halo},[length(zxy_shot),1]);     % ref about halo G
         zxy_temp=sum(zxy_temp.^2,2);            % evaluate radial distances
-        % TODO - radial mask around estimated centre
+        ind_halo=((zxy_temp<(((1+R_halo{i_halo})*CONFIGS.halo.R{i_halo})^2)) & (zxy_temp>(((1-R_halo{i_halo})*CONFIGS.halo.R{i_halo})^2)));  % halo counts index
+        zxy_halo{i_halo}=zxy_shot(ind_halo,:);  % counts in halo
+        zxy_shot=zxy_shot(~ind_halo,:);         % pop halo out
     end
     
     %% save single-shot to a cell
     BEC.zxy(i,:)=zxy_bec;   % BEC
-    HALO.zxy{i}=zxy_shot;   % TODO: this is a temporary solution - dump all remaining counts to halo
+    HALO.zxy(i,:)=zxy_halo;   % TODO: this is a temporary solution - dump all remaining counts to halo
+    CULLED.tail.zxy(i,:)=zxy_tail;  % tail
+    CULLED.fuzz.zxy{i}=zxy_shot;    % all remaining counts
+    
 end
-
