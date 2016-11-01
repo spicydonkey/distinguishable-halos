f4bdab0a62c36fbc4c2752cfdcabd614a670cf8d
David <dk.shin1992@gmail.com>
2016-11-01 15:04:57 +1100

    tidy main

--- dist_halo_main.m
+++ dist_halo_main.m
@@ -11,7 +11,8 @@ use_txy=1;          %if false will remake the txy_forc files
 verbose=2;
 
 % PLOTS
-% TODO
+% TODO - usr configurable switches for different plots (and req'd
+%   analysis?)
 
 % IN/OUTPUTS
 % files -  data file
@@ -19,20 +20,23 @@ usrconfigs.files.path='..\data\test\d';    % path to unindexed data file (e.g. '
 usrconfigs.files.id=1:100;         % file id numbers to use for analysis
 usrconfigs.files.minCount=100;     % min counts to use for analysis
 
-% windows - captures all the interesting counts in range: [] will not crop
-% TODO XY axis is unclear
+% miscellaneous
+usrconfigs.misc.vel_z=9.8*0.430;    % atom free-fall vert v at detector hit for T-to-Z conversion
+
+% WINDOW - captures only box-cropped counts in files ([]-->no crop)
+% TODO - XY axis is unclear
 usrconfigs.window.all_T=[4.905,4.92];       % T [s]
 usrconfigs.window.all{1}=[20.678,20.726];   % Z [m] T-window will overide Z
 usrconfigs.window.all{2}=[-20e-3,18e-3];    % X [m]
 usrconfigs.window.all{3}=[-10e-3,17e-3];    % Y [m]
 
-% DIST HALO PARAMS: params specific to data processing and analysis
+% DISTINGUISHABLE HALO PARAMS: params specific to data processing and analysis
 usrconfigs.bec.pos{1}=[20.7024,4.74e-3,2.72e-3];   % approx condensate locations (z,x,y)
 usrconfigs.bec.Rmax{1}=7e-3;  % max condensate sph radius
 usrconfigs.bec.rtail{1}=1.3;    % BEC tail rad ratio
 usrconfigs.bec.pos{2}=[20.7005,-7.38e-3,6.55e-3];
 usrconfigs.bec.Rmax{2}=7e-3;
-usrconfigs.bec.rtail{2}=1.3;    % BEC tail rad ratio
+usrconfigs.bec.rtail{2}=1.3;
 
 usrconfigs.halo.R{1}=11e-3;     % estimated radius of halo
 usrconfigs.halo.dR{1}=0.2;      % halo fractional thickness each dir (in/out)
@@ -47,13 +51,12 @@ configs=usrconfigs;    % create an alias to avoid overwriting user's config
 missingfiles=zeros(length(configs.files.id),1);     % missing dld/txy file
 filestotxy=zeros(length(configs.files.id),1);       % dld files processed to txy
 lowcountfiles=zeros(length(configs.files.id),1);    % files with too few counts (skipped in analysis)
-importokfiles=zeros(length(configs.files.id),1);    % successfully imported files
 
 
 %% Load processed data
 vars_saved = {'usrconfigs','configs'...
     'halo','bec','culled',...
-    'lowcountfiles','missingfiles','filestotxy','importokfiles',...
+    'lowcountfiles','missingfiles','filestotxy',...
     };  % variables important in halo analysis - saved to a .mat file
 
 if use_saved_data     
@@ -153,17 +156,6 @@ if ~use_saved_data
         end
     end
     
-    % Summary of TXY-preprocessing
-    importokfiles=~(missingfiles|lowcountfiles);
-    if verbose>1
-        disp('==============SUMMARY==============');
-        disp([int2str(sum(importokfiles)),' imported ok']);
-        disp([int2str(sum(lowcountfiles)),' files with low counts']);
-        disp([int2str(sum(missingfiles)),' files missing']);
-        disp([int2str(sum(filestotxy)),' files converted to txy']);
-        disp('===================================');
-    end
-    
     %% Process raw TXY data
     % This is where the raw TXY data (from DLD) must be processed to do further
     % analysis
@@ -172,20 +164,12 @@ if ~use_saved_data
     % SAVE the refined data and results
     % TODO: name vars appropriately and update the vars_saved, etc above
     
+    importokfiles=~(missingfiles|lowcountfiles);
     configs.files.idok=configs.files.id(importokfiles);     % ID's for OK files
     
     if verbose>0,disp('Processing TXY files to generate data for analysis...');,end;
     [halo,bec,culled]=distinguish_halo(configs,verbose);  % all the data processing on raw-TXY to generate halo data
     
-    % Summary
-    if verbose>0
-        % Calculated BEC centre
-        disp('============PROCESS SUMMARY============');
-        for i_mj=1:2
-            disp(['BEC ',num2str(i_mj),' centres: [',num2str(mean(vertcat(bec.cent{:,i_mj}))),']±[',num2str(std(vertcat(bec.cent{:,i_mj}))),']']);
-        end
-        disp('=======================================');
-    end
     
     %% Save processed data
     if verbose>0,disp('Saving data..');,end;
@@ -204,26 +188,60 @@ if ~use_saved_data
     end
 end
 
+%% SUMMARY
+% Summary of TXY-preprocessing
+importokfiles=~(missingfiles|lowcountfiles);
+if verbose>1
+    disp('===================IMPORT SUMMARY===================');
+    disp([int2str(sum(importokfiles)),' imported ok']);
+    disp([int2str(sum(lowcountfiles)),' files with low counts']);
+    disp([int2str(sum(missingfiles)),' files missing']);
+    disp([int2str(sum(filestotxy)),' files converted to txy']);
+    disp('====================================================');
+end
+
+% Summary
+if verbose>0
+    % Calculated BEC centre
+    disp('=================PROCESSING SUMMARY=================');
+    for i_mj=1:2
+        disp(['BEC ',num2str(i_mj),' centres: [',num2str(mean(vertcat(bec.cent{:,i_mj}))),']±[',num2str(std(vertcat(bec.cent{:,i_mj}))),']']);
+        %disp(['HALO ',num2str(i_mj),' centres: [',num2str(mean(vertcat(halo.cent{:,i_mj}))),']±[',num2str(std(vertcat(halo.cent{:,i_mj}))),']']);   % not really useful with this algorithm
+        % TODO - with optimisation, report halo radii
+    end
+    disp('====================================================');
+end
+
 
 %% DEBUG
-figure(111);
+% single-shot
+figure(111); title('Single shot');
 dot_size=100;
 scatter_zxy(111,(halo.zxy{1,1}),dot_size,'r');
 scatter_zxy(111,(halo.zxy{1,2}),dot_size,'b');
-scatter_zxy(111,(bec.zxy{1,1}),dot_size,'k');
-scatter_zxy(111,(bec.zxy{1,2}),dot_size,'k');
-scatter_zxy(111,(culled.tail.zxy{1,1}),dot_size,'g');
-scatter_zxy(111,(culled.tail.zxy{1,2}),dot_size,'g');
-scatter_zxy(111,(culled.fuzz.zxy{1}),dot_size,'g');
+% scatter_zxy(111,(bec.zxy{1,1}),dot_size,'k');
+% scatter_zxy(111,(bec.zxy{1,2}),dot_size,'k');
+% WARNING: don't plot zero-shifted with culled counts- they won't be
+%   shifted and will have problems with visulisation
+%
+% scatter_zxy(111,(culled.tail.zxy{1,1}),dot_size,'g');     
+% scatter_zxy(111,(culled.tail.zxy{1,2}),dot_size,'g');
+% scatter_zxy(111,(culled.fuzz.zxy{1}),dot_size,'g');
 
 figure(101); title('All shots combined');
 scatter_zxy(101,vertcat(halo.zxy{:,1}),1,'r');
 scatter_zxy(101,vertcat(halo.zxy{:,2}),1,'b');
-scatter_zxy(101,vertcat(bec.zxy{:,1}),1,'k');
-scatter_zxy(101,vertcat(bec.zxy{:,2}),1,'k');
-scatter_zxy(101,vertcat(culled.tail.zxy{:,1}),1,'g');
-scatter_zxy(101,vertcat(culled.tail.zxy{:,2}),1,'g');
-scatter_zxy(101,vertcat(culled.fuzz.zxy{:}),1,'g');
+% scatter_zxy(101,vertcat(bec.zxy{:,1}),1,'k');
+% scatter_zxy(101,vertcat(bec.zxy{:,2}),1,'k');
+% WARNING: don't plot zero-shifted with culled counts- they won't be
+%   shifted and will have problems with visulisation
+%
+% scatter_zxy(101,vertcat(culled.tail.zxy{:,1}),1,'g');
+% scatter_zxy(101,vertcat(culled.tail.zxy{:,2}),1,'g');
+% scatter_zxy(101,vertcat(culled.fuzz.zxy{:}),1,'g');
+
 
 %% Correlation analysis
 % find correlations
+
+toc;
