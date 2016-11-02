2e4025eb7f1a3e5c4a5f07229e245e68d004ddf4
David <dk.shin1992@gmail.com>
2016-11-02 16:25:32 +1100

    implement +/- disp to zxy_translate.m

--- zxy_translate.m
+++ zxy_translate.m
@@ -1,15 +1,20 @@
-function ZXY_OUT = zxy_translate(ZXY_IN,DISP)
+function ZXY_OUT = zxy_translate(ZXY_IN,DISP,SIGN)
 % Translates uniformly all ZXY-count of each shot by the correponding displacement vector
 % DKS 1/11/16
 %
 % I/O
 % ZXY_IN: should be a Nx1 cell of ZXY-array
 % DISP: Nx1 cell of a single ZXY displacement vector
+% SIGN: +/-1 adds/subtracts displacement (default=ADDITIVE)
+
+if ~exist('SIGN','var')
+    SIGN=1;     % default sign is ADDITION
+end
 
 nShot=size(ZXY_IN,1);
 
 ZXY_OUT=cell(nShot,1);
 for i=1:nShot
     nCount=size(ZXY_IN{i},1);
-    ZXY_OUT{i}=ZXY_IN{i}+repmat(DISP{i},[nCount,1]);
+    ZXY_OUT{i}=ZXY_IN{i}+SIGN*repmat(DISP{i},[nCount,1]);
 end
