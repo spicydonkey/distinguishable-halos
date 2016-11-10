d19f409fb9356ce4dd71827880c48ea831a0ee5b
David <dk.shin1992@gmail.com>
2016-11-10 11:22:00 +1100

    Fix major bug in zxy --> polar conversion: need to use 4-quadrant transformation

--- zxy2pol.m
+++ zxy2pol.m
@@ -12,7 +12,7 @@ SPH_OUT=cell(nShot,1);
 for i=1:nShot
     zxy_tmp=ZXY_IN{i};
     rad_tmp=sqrt(sum(zxy_tmp.^2,2));    % radius
-    azim_tmp=atan(zxy_tmp(:,3)./zxy_tmp(:,2));  % azimuthal ang=atan(Y/X)
+    azim_tmp=atan2(zxy_tmp(:,3),zxy_tmp(:,2));  % azimuthal ang=atan2(Y/X) - 4-quadrant inverse tangent
     elev_tmp=asin(zxy_tmp(:,1)./rad_tmp);       % elevation ang=asin(Z/NORM)
     
     SPH_OUT{i}=[rad_tmp, azim_tmp, elev_tmp];   % SPHPOL=(R,azim,elev)
