ce7c5a7641e0908ff29894aa8d9a817cbff545b2
David <dk.shin1992@gmail.com>
2016-11-15 11:32:35 +1100

    create a generic G2 caller

---
+++ G2_caller.m
@@ -0,0 +1,24 @@
+function [G2_SINGLE,G2_ALL]=G2_caller(DATA,BIN_EDGE,TYPE,VERBOSE)
+% Wrapper to call different G2 routines
+%
+% INPUT
+% TYPE: must be a string 'cart' or 'polar'
+%
+% OUTPUT: see G2_cart.m or G2_angular.m
+%
+
+if ~exist('TYPE','var')
+    error('Specify TYPE for G2_caller: must be either "cart" of "angular".');
+end
+if ~(isequal(TYPE,'cart')||isequal(TYPE,'angular'))
+    error('TYPE must be either "cart" or "angular".');
+end
+
+if isequal(TYPE,'cart')
+    [G2_SINGLE,G2_ALL]=G2_cart(DATA,BIN_EDGE,VERBOSE);
+elseif isequal(TYPE,'angular')
+    [G2_SINGLE,G2_ALL]=G2_angular(DATA,BIN_EDGE,VERBOSE);
+else
+    % some other routine would go here
+    return
+end
