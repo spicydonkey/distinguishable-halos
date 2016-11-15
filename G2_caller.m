cab64dc0f09b592fa66d181e3d0e69fad514cdce
David <dk.shin1992@gmail.com>
2016-11-15 12:06:30 +1100

    minor fix for cartesian optional argument

--- G2_caller.m
+++ G2_caller.m
@@ -1,4 +1,4 @@
-function [G2_SINGLE,G2_ALL]=G2_caller(DATA,BIN_EDGE,TYPE,VERBOSE)
+function [G2_SINGLE,G2_ALL]=G2_caller(DATA,BIN_EDGE,TYPE,OPTIONAL,VERBOSE)
 % Wrapper to call different G2 routines
 %
 % INPUT
@@ -15,7 +15,7 @@ if ~(isequal(TYPE,'cart')||isequal(TYPE,'angular'))
 end
 
 if isequal(TYPE,'cart')
-    [G2_SINGLE,G2_ALL]=G2_cart(DATA,BIN_EDGE,VERBOSE);
+    [G2_SINGLE,G2_ALL]=G2_cart(DATA,BIN_EDGE,OPTIONAL,VERBOSE);
 elseif isequal(TYPE,'angular')
     [G2_SINGLE,G2_ALL]=G2_angular(DATA,BIN_EDGE,VERBOSE);
 else
