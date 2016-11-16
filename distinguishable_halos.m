ac0025fb7d5b25b49208f995fac3e2650a45bdcb
David <dk.shin1992@gmail.com>
2016-11-17 10:53:48 +1100

    add the skipping feature on processes

--- distinguishable_halos.m
+++ distinguishable_halos.m
@@ -9,11 +9,13 @@
 % [] analysis
 
 % GENERAL
+force_all_stages=0;    % force all the stages to run (useful for debug)
+
 verbose=2;
 
 % files -  data file
 usrconfigs.files.path='C:\Users\HE BEC\Documents\lab\halo_analysis\data\dist_halo\4_separated_lownum\d';    % path to unindexed data file (e.g. 'a\b\datadir\$DATA_FNAME_TOKEN$')
-usrconfigs.files.id=1:3000;         % file id numbers to use for analysis
+usrconfigs.files.id=1:100;         % file id numbers to use for analysis
 usrconfigs.files.minCount=100;     % min counts to use for analysis
 
 % TXY window - region of interest ( [] --> no crop )
@@ -21,6 +23,12 @@ usrconfigs.window{1}=[4.905,4.92];      % T [s]
 usrconfigs.window{2}=[-20e-3,18e-3];    % X [m]
 usrconfigs.window{3}=[-10e-3,17e-3];    % Y [m]
 
+%%% ALGORITHM CONFIGS
+% DO NOT ADJUST
+usrconfigs.files.saveddata=[usrconfigs.files.path,'_data.mat'];     % path to store saved data
+usrconfigs.files.archive=[usrconfigs.files.path,'_archive'];   % dir to archive folder
+
+do_next=force_all_stages;  % flag for executing stages for efficiency (do not change)
 
 configs=usrconfigs;     % copy to protect usrconfigs
 
@@ -28,11 +36,31 @@ configs=usrconfigs;     % copy to protect usrconfigs
 %   <<Skip if already done and configs are same>>
 %   Process rawDLD/TXY archive to:
 %     - get counts separated by shot# (ditch low-counts)
-%     - save (ZXY)-counts to .mat file
+%     - save (ZXY)-counts to .mat file: $txy_all
+%     - $files contains summary of each TXY file
 
-% TODO - implement the skip feature
-loadExpData(configs,verbose);
+% Check if can be skipped
+if ~do_next
+    % check for existing saved data file
+    if exist(configs.files.saveddata,'file')
+        % same configs?: files, window
+        % TODO - will throw error if $configs is not a variable in data
+        S_temp=load(configs.files.saveddata,'configs');  % load configs from prev data
+        % TODO - will throw error if $files and $window are not fields of $configs
+        if ~(isequal(S_temp.configs.files,configs.files)&&isequal(S_temp.configs.window,configs.window))
+            warning('Existing data has different configs. Setting do_next=1.');
+            do_next=1;
+        end
+    else
+        warning('No existing data. Setting do_next=1.');
+        do_next=1;
+    end
+    clear S_temp;
+end
 
+if do_next
+    loadExpData(configs,verbose);
+end
 
 %% Preprocess: Get halo counts
 %   <<Skip if already done AND configs are same>>
@@ -42,6 +70,7 @@ loadExpData(configs,verbose);
 %       a broad radial mask
 %     - collate all shots with BEC oscillations cancelled (esp. halo)
 %     - save to file (think about not saving BEC's - too many counts)
+
 
 
 
