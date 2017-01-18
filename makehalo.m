3be4ad1f1154cc3ac3125bbc160cc44ac072d771
David <dk.shin1992@gmail.com>
2017-01-18 12:47:23 +1100

    create s-wave halo generator with description

---
+++ makehalo.m
@@ -0,0 +1,14 @@
+%% Function to generate a pair of s-wave scattered halos
+% DK SHIN
+% 18/01/2017
+
+
+%% I/O
+% Inputs:
+%   Nsource     {num atoms in collision; per colliding source}
+%   det_eff     {detection efficiency}
+%   bgnd        {background counts}
+%   p_width     {momentum width/uncert of source; Gaussian, symmetric}
+
+% Outputs:
+%   scat_prod   {scattering proudcts}   %data structure?
