51f0c3b32a03e789b620292a8c99763d1e495bd1
David <dk.shin1992@gmail.com>
2017-02-22 17:47:30 +1100

    new feature: number of counts to plot

--- plot_zxy.m
+++ plot_zxy.m
@@ -2,10 +2,13 @@
 % Plots Nshot-by-Mspecies cell of ZXY counts
 % DKS 
 
-function plot_zxy(ZXY,SIZE,COLORS)
+function plot_zxy(ZXY,NDISP,SIZE,COLORS)
 if ~iscell(ZXY)
     warning('ZXY must be a cell array.');
     return;
+end
+if ~exist('NDISP','var')
+    NDISP=[];
 end
 if ~exist('COLORS','var')
     COLORS='brgmcyk';  % default COLORS
@@ -17,7 +20,19 @@ end
 hold on;    % hold current figure - to plot on
 n_species=size(ZXY,2);
 for i=1:n_species
-    temp_zxy=vertcat(ZXY{:,i});
+    temp_zxy=vertcat(ZXY{:,i});     % whole collated data
+    
+    if ~isequal(NDISP,[])
+        % randomly filter counts to display
+        N_TOT=size(temp_zxy,1);
+        idx_sel=rand(N_TOT,1);          % RNG array
+        idx_sel=idx_sel<(NDISP/N_TOT);  % RNG to binary selection indices
+        
+        disp(sum(idx_sel));
+        
+        temp_zxy=temp_zxy(idx_sel,:);
+    end
+    
     scatter_zxy(temp_zxy,SIZE,COLORS(i));
 end
 
