9f16b9404aaa0dd7a3de66ed94ce128ba0a4bd98
David <dk.shin1992@gmail.com>
2017-06-15 12:05:59 +1000

    update analysis for run2

--- mode_occupation.m
+++ mode_occupation.m
@@ -1,8 +1,15 @@
-% RUN 1
+% % RUN 1
+% kr=1;       % halo radius
+% wbb=0.03;   % bb correlation length
+% dk=0.027;   % halo width rms
+% Nsc=45;  % number scattered
+
+% RUN2
 kr=1;       % halo radius
 wbb=0.03;   % bb correlation length
-dk=0.027;   % halo width rms
-Nsc=45;  % number scattered
+dk=0.04;   % halo width rms
+Nsc=120;  % number scattered
+
 
 Vm=(2*pi)^(3/2)*(wbb/1.1)^3        % mode/phase grain volume
 Vs=4*pi*sqrt(2*pi)*kr^2*dk         % total scattering volume
