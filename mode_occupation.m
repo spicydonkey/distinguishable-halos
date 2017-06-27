c4adfecfb6466f1f5300104d16e62c232d9d3098
David <dk.shin1992@gmail.com>
2017-06-27 13:24:59 +1000

    create config for new dataset

--- mode_occupation.m
+++ mode_occupation.m
@@ -4,11 +4,17 @@
 % dk=0.027;   % halo width rms
 % Nsc=45;  % number scattered
 
-% RUN2
-kr=1;       % halo radius
-wbb=0.03;   % bb correlation length
-dk=0.04;   % halo width rms
-Nsc=120;  % number scattered
+% % RUN2
+% kr=1;       % halo radius
+% wbb=0.03;   % bb correlation length
+% dk=0.027;   % halo width rms
+% Nsc=120;  % number scattered
+
+% 20170622
+kr=1;
+wbb=0.045;
+dk=0.06;
+Nsc=315;
 
 
 Vm=(2*pi)^(3/2)*(wbb/1.1)^3        % mode/phase grain volume
