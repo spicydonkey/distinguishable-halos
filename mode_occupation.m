c45e1eef974e5ae5d1044ef0d6ca3b51a1c1c77f
David <dk.shin1992@gmail.com>
2017-06-27 18:06:51 +1000

    new configs + update mode occupation

--- mode_occupation.m
+++ mode_occupation.m
@@ -6,15 +6,27 @@
 
 % % RUN2
 % kr=1;       % halo radius
-% wbb=0.03;   % bb correlation length
-% dk=0.027;   % halo width rms
+% wbb=0.028;   % bb correlation length
+% dk=0.04;   % halo width rms
 % Nsc=120;  % number scattered
 
-% 20170622
-kr=1;
-wbb=0.045;
-dk=0.06;
-Nsc=315;
+% % 20170622
+% kr=1;
+% wbb=0.045;
+% dk=0.06;
+% Nsc=315;
+
+% % 20170624 --> BAD
+% kr=1;
+% wbb=0.03;   % TODO
+% dk=0.1;
+% Nsc=110;
+
+% % 20170627
+% kr=1;
+% wbb=0.03;       % TODO
+% dk=0.12;
+% Nsc=38;
 
 
 Vm=(2*pi)^(3/2)*(wbb/1.1)^3        % mode/phase grain volume
