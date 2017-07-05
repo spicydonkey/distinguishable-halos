183ee9e1d1364167567c3ecf1d1db22f9fc8db6a
spicydonkey <dk.shin1992@gmail.com>
2017-07-05 23:37:37 +1000

    update mf1 collision conflig

--- config/config_mf1.m
+++ config/config_mf1.m
@@ -5,7 +5,7 @@ configs.flags.do_corr_analysis=0;
 configs.flags.force_all_stages=0;    % force all the stages to run (useful for debug)
 configs.flags.verbose=2;
 configs.flags.savedata=0;       % TODO - req'd currently since each stage passes data by save/load to disk
-configs.flags.archive_txy=0;        % archives loaded TXY as .mat file for future reuse
+configs.flags.archive_txy=1;        % archives loaded TXY as .mat file for future reuse
 configs.flags.graphics=1;       % toggle to control graphics/plotting options
 configs.flags.build_txy=1;
 
@@ -15,7 +15,7 @@ vz=configs.misc.vel_z;
 
 
 %% FILES
-configs.files.path='\\AMPLPC29\Users\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\xstate_collision_highv\mf1_collision\d';
+configs.files.path='C:\Users\David\Documents\hebec\halo_analysis\data\dist_halo\mf1_collision\d';
 
 % WARNING: MODIFYING BELOW DIR SETTINGS ARE NOT RECOMMENDED
 configs.files.dir_data=fileparts(configs.files.path);    % fullpath to data directory
@@ -46,43 +46,43 @@ configs.load.window{3}=[-35e-3,35e-3];    % Y [m]
 
 %% HALO
 %%% HALO PARAMS: BEC counts + oscillation removal for broad capture of halos
-configs.bec.pos{1}=[2.444*vz,-1.7e-3,-0.8e-3];   % approx condensate locations (z,x,y)
-configs.bec.Rmax{1}=6e-3;      % max condensate sph radius
+configs.bec.pos{1}=[9.9715,-1.9e-3,-0.5e-3];   % approx condensate locations (z,x,y)
+configs.bec.Rmax{1}=8e-3;      % max condensate sph radius
 configs.bec.dR_tail{1}=1;     % BEC tail radial frac diff
-configs.bec.pos{2}=[2.456*vz,-2.3e-3,0.5e-3];
-configs.bec.Rmax{2}=6e-3;
+configs.bec.pos{2}=[10.021,-2.2e-3,0.6e-3];
+configs.bec.Rmax{2}=8e-3;
 configs.bec.dR_tail{2}=1;
 
-% configs.halo.R{1}=26e-3;     % estimated radius of halo
-% configs.halo.dR{1}=0.25;      % broad radial mask fractional width (in/out)
+configs.halo.R{1}=24e-3;     % estimated radius of halo
+configs.halo.dR{1}=0.3;      % broad radial mask fractional width (in/out)
 % configs.halo.R{2}=24e-3;
 % configs.halo.dR{2}=0.25;
 
-% configs.halo.zcap=0.6;   % z-cutoff (fractional wrt radius)
+configs.halo.zcap=0.75;   % z-cutoff (fractional wrt radius)
 
 % TODO - does boost need to be optimised for different g2 analysis?
 %   currently SINGLE boost applied to halo2 to obtain best g2_01_BB
 % configs.halo.boost{1}=zeros(1,3);
 % configs.halo.boost{2}=[0.035,-0.002,-0.025];
 % 
-% %% CORRELATION ANALYSIS
-% % 3) Single-halo cart CL - m_J=0
-% configs.corr{3}.type.comp=1;
-% configs.corr{3}.type.coord='cart';
-% configs.corr{3}.type.opt='CL';
-% configs.corr{3}.lim=0.2*repmat([0,1],[3,1]);
-% configs.corr{3}.nBin=11*[1,1,1];   % number of bins
-% 
-% % 5) Single-halo cart BB - m_J=0
-% configs.corr{5}.type.comp=1;
-% configs.corr{5}.type.coord='cart';
-% configs.corr{5}.type.opt='BB';
-% configs.corr{5}.lim=0.2*repmat([-1,1],[3,1]);
-% configs.corr{5}.nBin=11*[1,1,1];   % number of bins
-% 
-% % 8) Single-halo Angular - m_J=0
-% configs.corr{8}.type.comp=1;           
-% configs.corr{8}.type.coord='angular';
-% configs.corr{8}.type.opt=[];
-% configs.corr{8}.lim=[0,0.2;0,pi];
-% configs.corr{8}.nBin=[21,501];
+%% CORRELATION ANALYSIS
+% 1) Single-halo Angular - m_J=0
+configs.corr{1}.type.comp=1;           
+configs.corr{1}.type.coord='angular';
+configs.corr{1}.type.opt=[];
+configs.corr{1}.lim=[0,0.2;0,pi];
+configs.corr{1}.nBin=[21,501];
+
+% 2) Single-halo cart CL - m_J=0
+configs.corr{2}.type.comp=1;
+configs.corr{2}.type.coord='cart';
+configs.corr{2}.type.opt='CL';
+configs.corr{2}.lim=0.2*repmat([0,1],[3,1]);
+configs.corr{2}.nBin=11*[1,1,1];   % number of bins
+
+% 3) Single-halo cart BB - m_J=0
+configs.corr{3}.type.comp=1;
+configs.corr{3}.type.coord='cart';
+configs.corr{3}.type.opt='BB';
+configs.corr{3}.lim=0.2*repmat([-1,1],[3,1]);
+configs.corr{3}.nBin=11*[1,1,1];   % number of bins
