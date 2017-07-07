df333c496e9d79e10ae5bd28617b6a35868ac098
David <dk.shin1992@gmail.com>
2017-07-07 15:36:46 +1000

    xy errorbars and annotate

--- summary_n_vs_g2.m
+++ summary_n_vs_g2.m
@@ -1,28 +1,46 @@
 % SUMMARY OF BB g2 amplitude and scattering halo mode occupancy
 
+%% DATA
 n_exp=[0.017,0.015,0.04,0.86,0.09,2.3e-3,0.3];
 g2_exp=[85,22,32,3.1,16,140,7];
+
+% UNCERTAINTY
+n_unc=0.8;      % simple ±80% uncertainty
+n_err=n_exp*n_unc;      % calculated
+
+% g2_err
+g2_unc=0.5;     % simple
+g2_err=g2_exp*g2_unc;
 
 r=3;
 n=linspace(min(n_exp)/r,r*max(n_exp),1000);       % logs
 % n=linspace(0,1.1*max(n_exp),1000);       % linear
 g2_theory=1+1./n;
 
-% Plot
+%% Plot
+% plot configs
+namearray={'LineWidth','MarkerFaceColor'};
+valarray={1.5,'w'};
+
 % plot mode occupancy vs g2(0) - 1
 figure();
-plot(n_exp,g2_exp-1,'*');
 hold on;
-plot(n,g2_theory-1);
+htheory=plot(n,g2_theory-1,'r--','LineWidth',1.5,'DisplayName','Theory');       % theory curve
+
+hdata=ploterr(n_exp,g2_exp-1,n_err,g2_err,'ko','logxy','hhxy',0);
+set(hdata(1),namearray,valarray,'DisplayName','Data');              % DATAPOINT
+set(hdata(2),namearray,valarray,'DisplayName','');                  % Y-err
+set(hdata(3),namearray,valarray,'DisplayName','');                  % X-err
+
+% annotate
+legend([htheory,hdata(1)]);
 
 ax=gca;
-
-% ax.XScale='lin';
-% ax.YScale='lin';
 ax.XScale='log';
 ax.YScale='log';
 
 axis tight;
+box on;
 grid on;
 xlabel('$n$');
 ylabel('$g^{(2)}(0)-1$');
