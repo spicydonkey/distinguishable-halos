f30c2256f372eeb288f4d8decbf8ffdc809d1c4e
David <dk.shin1992@gmail.com>
2017-07-11 12:03:47 +1000

    update all error analysis'

--- summary_n_vs_g2.m
+++ summary_n_vs_g2.m
@@ -1,23 +1,26 @@
 % SUMMARY OF BB g2 amplitude and scattering halo mode occupancy
 
 %% CONFIGS
-% plotting
-n_lim=[2e-3,2e0];       % axes lims for mode occupancy
-g2_lim=[3e-1,3e2];      % axes lims for g2-1
+% plotting limits
+n_lim=[2e-3,1e0];       % axes lims for mode occupancy
+g2_lim=[1e0,2e2];      % axes lims for g2-1
 
-%%% DATA
-%% 90 deg data
-n_exp=[0.0080, 0.0156, 0.0389, 0.604, 0.086, 0.011, 0.273];
-g2_exp=[87, 22, 32, 3.6, 20, 95, 8];
+% condition for violoation of Bell's test
+g2_bell=(2*sqrt(2)+3)-1;            % Wasak, Chwedenczuk - limit g2 - 1
 
-% UNCERTAINTY
+
+%% DATA
+%%% 90 deg data
+n_exp=[0.0085, 0.0126, 0.0575, 0.604, 0.0887, 0.0102, 0.273];
+g2_exp=[92.5, 23.3, 31.5, 3.6, 20.3, 97.3, 8];      % mean fit amplitude
+
+% uncertainties
 n_unc=[0.68, 1.1, 0.38, 0.24, 0.39, 0.65, 0.32];      % relative error in ratio to mean
 
-% g2_err
-% g2_unc=0.75;     % simple error ratio
-g2_err=[0, 0, 0, 0, 0, 0, 0];
+g2_err=[9.1, 1.2, 2.1, 0.15, 0.5, 8.5, 0.43];   % error in SE
+g2_err=g2_err*sqrt(5);               % error in SD
 
-% %% 30 deg data
+% %%% 30 deg data
 % n_exp=[n_exp 0.224];
 % g2_exp=[g2_exp 7.5];
 %     
@@ -25,9 +28,8 @@ g2_err=[0, 0, 0, 0, 0, 0, 0];
 % g2_err=[g2_err 1.0];
 
 %% EVALUATE error limits
-% relative error
+% relative error to abs
 n_err=n_exp.*n_unc;      % calculated
-% g2_err=g2_exp*g2_unc;
 
 % error bounds
 ndata=numel(n_exp);         % number of data points
@@ -41,7 +43,7 @@ for ii=1:2
     g2_err_bnd{ii}=g2_err_bnd{ii}-1;            % shift to g2-1
 end
 
-% cut bound at axes lims (MIN only)
+% cut bound at axes lims (MIN only for log plot)
 for ii=1:ndata
     if n_err_bnd{1}(ii)<n_lim(1)
         n_err_bnd{1}(ii)=n_lim(1);
@@ -51,7 +53,7 @@ for ii=1:ndata
     end
 end
 
-%% Theory
+%% Theory curve
 r=3;
 n=linspace(min(n_exp)/r,r*max(n_exp),1000);       % logs
 % n=linspace(0,1.1*max(n_exp),1000);       % linear
@@ -66,7 +68,7 @@ valarray={1.5,'w'};
 % plot mode occupancy vs g2(0) - 1
 figure();
 hold on;
-htheory=plot(n,g2_theory-1,'r--','LineWidth',1.5,'DisplayName','Theory');       % theory curve
+htheory=plot(n,g2_theory-1,'k--','LineWidth',1.5,'DisplayName','Theory');       % theory curve
 
 % hdata=ploterr(n_exp,g2_exp-1,n_err,g2_err,'ko','logxy','hhxy',0);
 hdata=ploterr(n_exp,g2_exp-1,n_err_bnd,g2_err_bnd,'ko','logxy','hhxy',0);
@@ -75,6 +77,8 @@ set(hdata(2),namearray,valarray,'DisplayName','');                  % Y-err
 set(hdata(3),namearray,valarray,'DisplayName','');                  % X-err
 
 % annotate plot
+box on;
+grid on;
 legend([htheory,hdata(1)]);
 
 ax=gca;
@@ -84,9 +88,18 @@ ax.YScale='log';
 xlim(n_lim);
 ylim(g2_lim);
 
-axis tight;
-box on;
-grid on;
 xlabel('$n$');
 ylabel('$g^{(2)}(0)-1$');
-axis tight;
+
+axis square;        % square plot
+
+%% Bell's test regime
+% build patch corners
+x_corn=[n_lim(1),n_lim(2),n_lim(2),n_lim(1)];
+% y_corn=[g2_lim(1),g2_lim(1),g2_bell,g2_bell];     % below condition
+y_corn=[g2_bell,g2_bell,g2_lim(2),g2_lim(2)];       % above condition
+
+p_bell=patch(x_corn,y_corn,...
+    'b','FaceAlpha',0.1,'EdgeColor','none');
+
+uistack(p_bell,'bottom');   % move the patch object to bottom
