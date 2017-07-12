c2eb638fe514b7fba61dff7d0e68f98f10f90855
David <dk.shin1992@gmail.com>
2017-07-12 12:55:24 +1000

    manage fig annotation/format + add new data points

--- summary_n_vs_g2.m
+++ summary_n_vs_g2.m
@@ -1,5 +1,24 @@
 % SUMMARY OF BB g2 amplitude and scattering halo mode occupancy
 
+%% Figure properties presets
+figname = 'g2_vs_mocc';
+fontsize_normal = 12;
+fontsize_small=9;
+linewidth = 1.5;
+% markersize = 5;
+% plotcolor = {'b','r'};
+
+% paperunits = 'centimeters';
+% papersize = [8 11];
+% paperposition = [0 0 papersize];
+
+% MISC
+gray_col=0.5*ones(1,3);         % gray data points
+namearray={'LineWidth','MarkerFaceColor','Color'};      % error bar graphics properties
+valarray={linewidth,'w','k'};                 % 90 deg (normal) data
+valarray_30={linewidth,gray_col,gray_col};    % 30 deg (slow) data
+
+
 %% CONFIGS
 % plotting limits
 n_lim=[2e-3,1e0];       % axes lims for mode occupancy
@@ -11,21 +30,21 @@ g2_bell=(2*sqrt(2)+3)-1;            % Wasak, Chwedenczuk - limit g2 - 1
 
 %% DATA
 %%% 90 deg data
-n_exp=[0.0085, 0.0126, 0.0575, 0.604, 0.0887, 0.0102, 0.273];
-g2_exp=[92.5, 23.3, 31.5, 3.6, 20.3, 97.3, 8];      % mean fit amplitude
+n_exp=[0.0085, 0.0126, 0.0575, 0.604, 0.0887, 0.0102, 0.273 0.052];
+g2_exp=[92.5, 23.3, 31.5, 3.6, 20.3, 97.3, 8 26.6];      % mean fit amplitude
 
 % uncertainties
-n_unc=[0.68, 1.1, 0.38, 0.24, 0.39, 0.65, 0.32];      % relative error in ratio to mean
+n_unc=[0.68, 1.1, 0.38, 0.24, 0.39, 0.65, 0.32 0.84];      % relative error in ratio to mean
 
-g2_err=[9.1, 1.2, 2.1, 0.15, 0.5, 8.5, 0.43];   % error in SE
+g2_err=[9.1, 1.2, 2.1, 0.15, 0.5, 8.5, 0.43 1.5];   % error in SE
 
 
 %%% 30 deg data
-n_exp_30=[0.224 0.577 0.82];
-g2_exp_30=[7.5 3.7 2.6];
+n_exp_30=[0.196 0.462 0.774 0.299];
+g2_exp_30=[7.6 3.6 2.73 5.9];
     
-n_unc_30=[0.46 0.31 0.28];
-g2_err_30=[1.0 0.27 0.1];
+n_unc_30=[0.44 0.38 0.24 0.4];
+g2_err_30=[1.0 0.16 0.06 0.41];
 
 
 % Use SD for g2 uncertainty
@@ -68,14 +87,9 @@ g2_theory=1+1./n;
 
 
 %% Plot
-% plot configs
-gray_col=0.5*ones(1,3);
-namearray={'LineWidth','MarkerFaceColor','Color'};
-valarray={1.5,'w','k'};
-valarray_30={1.5,gray_col,gray_col};
 
 % plot mode occupancy vs g2(0) - 1
-figure();
+fig=figure();
 hold on;
 htheory=plot(n,g2_theory-1,'k--','LineWidth',1.5,'DisplayName','Theory');       % theory curve
 
@@ -94,7 +108,8 @@ set(hdata(3),namearray,valarray,'DisplayName','');                  % X-err
 % annotate plot
 box on;
 grid on;
-legend([hdata(1),hdata_30(1),htheory]);
+oleg=legend([hdata(1),hdata_30(1),htheory]);
+set(oleg,'FontSize',fontsize_small);
 
 ax=gca;
 ax.XScale='log';
@@ -118,3 +133,17 @@ p_bell=patch(x_corn,y_corn,...
     'b','FaceAlpha',0.1,'EdgeColor','none');
 
 uistack(p_bell,'bottom');   % move the patch object to bottom
+
+
+%% FIG postprocess
+set(ax,'FontSize',fontsize_normal);
+
+% fig.Units = paperunits;
+% fig.Position = paperposition;
+% 
+% fig.PaperSize = papersize;
+% fig.PaperUnits = paperunits;
+% fig.PaperPosition = paperposition;
+
+% saveas(fig, [figname, '.eps'], 'psc2');     % save fig in cd
+saveas(fig, [figname, '.pdf']);
